2a7e52d68354cac99c0f9db6eac38f531fa05e8a
Tyler Long <tyler4long@gmail.com>
2017-03-11 17:46:40 +0800

    Add config.ru

--- index.rb
+++ index.rb
@@ -4,6 +4,10 @@ require 'json'
 require 'dotenv'
 Dotenv.load
 
+get '/' do
+  'Hello world'
+end
+
 post '/:uuid' do |uuid|
   url = "#{ENV['webhook_base_url']}/#{uuid}"
   RestClient.post url, JSON.parse(request.body.read.to_s), headers
